2dc61053432ec69ea8a281e6cf666c5a92d7a12b
Iago Moreira <iago.rn@gmail.com>
2011-06-26 21:30:28 -0300

    Usando IP do request

--- src/countryInfoFromIP/CountryIPServlet.java
+++ src/countryInfoFromIP/CountryIPServlet.java
@@ -8,7 +8,7 @@ public class CountryIPServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 	throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println(( new CountryIPImpl()).getCapitalFromCountryIP("173.184.132.20"));
+		resp.getWriter().println(( new CountryIPImpl()).getCapitalFromCountryIP(req.getRemoteAddr()) );
 	}
-
+	
 }
