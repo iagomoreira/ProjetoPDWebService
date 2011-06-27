93043a3feb9b9600734b10ea35d1eb1ac102990c
Iago Moreira <iago.rn@gmail.com>
2011-06-26 21:46:20 -0300

    Refatorando o RESTCountryIP

--- src/countryInfoFromIP/RESTCountryIP.java
+++ src/countryInfoFromIP/RESTCountryIP.java
@@ -12,27 +12,27 @@ import javax.xml.ws.http.HTTPException;
 import javax.annotation.Resource;
 
 @WebServiceProvider
-@ServiceMode( value = javax.xml.ws.Service.Mode.MESSAGE)
-@BindingType( value = HTTPBinding.HTTP_BINDING)
+@ServiceMode(value = javax.xml.ws.Service.Mode.MESSAGE)
+@BindingType(value = HTTPBinding.HTTP_BINDING)
 public class RESTCountryIP implements Provider<Source>{
 
 	@Resource
-	protected WebServiceContext webServiceContext;
+	protected WebServiceContext ws_ctx;
 	protected static final CountryIPImpl countryIP = new CountryIPImpl();
 	
 	@Override
 	public Source invoke(Source arg0) {
 		
-		if( webServiceContext == null ){
-			throw new RuntimeException();
+		if( ws_ctx == null ){
+			throw new RuntimeException("Injection failed on ws_ctx");
 		}
 		
-		MessageContext msgContext = webServiceContext.getMessageContext();
-		String metodoHTTP = (String) msgContext.get(MessageContext.HTTP_REQUEST_METHOD);
-		metodoHTTP = metodoHTTP.toUpperCase().trim();
+		MessageContext msg_ctx = ws_ctx.getMessageContext();
+		String http_verb = (String) msg_ctx.get(MessageContext.HTTP_REQUEST_METHOD);
+		http_verb = http_verb.trim().toUpperCase();
 		
-		if( metodoHTTP.equals("GET")){
-			return get(msgContext);
+		if( http_verb.equals("GET")){
+			return doGet(msg_ctx);
 		}
 		else{
 			throw new HTTPException(405);
@@ -40,10 +40,11 @@ public class RESTCountryIP implements Provider<Source>{
 		
 	}
 	
-	private Source get( MessageContext msg ){
-		String qString = ( String ) msg.get(MessageContext.QUERY_STRING);
-		String[] particaoQString = qString.split("=");
+	private Source doGet( MessageContext msg_ctx ){
+		String query_string = ( String ) msg_ctx.get(MessageContext.QUERY_STRING);
+		String[] particaoQString = query_string.split("=");
 		String parametro;
+		
 		if( particaoQString[0].toLowerCase() != "ip"){
 			throw new HTTPException(400);
 		}
@@ -51,12 +52,12 @@ public class RESTCountryIP implements Provider<Source>{
 			parametro = particaoQString[1].trim();
 		}
 		
-		ByteArrayInputStream inputStream = objToStream(countryIP.getCapitalFromCountryIP(parametro));
+		ByteArrayInputStream inputStream = encode_to_stream(countryIP.getCapitalFromCountryIP(parametro));
 		
 		return new StreamSource(inputStream);		
 	}
 	
-	private ByteArrayInputStream objToStream( Object object ){
+	private ByteArrayInputStream encode_to_stream( Object object ){
 		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
 		XMLEncoder xmlEnc = new XMLEncoder(outStream);
 		
